3e2c01339301e23ae9bf4435222a916bd5599109
aetheryx <zainalih3c@gmail.com>
2018-12-25 00:07:17 +0100

    coughs

--- src/routes/spotifyCallback.js
+++ src/routes/spotifyCallback.js
@@ -8,7 +8,7 @@ module.exports = (app, config, db) =>
       return res.status(412).send(`No user profile found in session. Start at <a href="/link">${config.domain}/link<a>.<br>If the issue persists, please join <a href="https://discord.gg/Yphr6WG">Powercord's support server</a> for assistance.`);
     }
 
-    const token = await SpotifyOAuth.getToken('aaa' ).catch(next);
+    const token = await SpotifyOAuth.getToken(req.query.code).catch(next);
     const user = await SpotifyOAuth.getUserByBearer(token.access_token).catch(next);
     
     const jwt = await encode({ id: req.session.user.id });
