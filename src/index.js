384119f576554f38d05c0749d09315085946ec0c
Bowser65 <contact.bowser65@gmail.com>
2018-12-24 12:04:09 +0100

    Begin rewrite

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
+const { resolve, join } = require('path');
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
-const config = require('./config.json');
-const routes = require('./routes/');
+const config = require('../config.json');
+const routes = require('./routes');
 const getDB = require('./db.js');
 
 (async () => {
   const db = await getDB();
   const app = express();
 
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.set('view engine', 'ejs');
+  app.set('views', join(__dirname, '/views'));
+
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }));
+  app.use(bodyParser.json());
   app.use(session({
     secret: config.secret,
     saveUninitialized: true,
     resave: false
   }));
 
-  for (const route of routes) {
-    route.call(this, app, config, db);
-  }
+  app.use('/assets', express.static(resolve(__dirname, '..', 'static')))
+  routes.call(this, app, config, db);
 
   app.listen(config.port, () => {
     console.log(`Express server listening to ${config.port}.`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,18 @@
+// const { SpotifyOAuth } = require('../rest/');
+// const { decode } = require('../util/jwt.js');
+
+module.exports = (app, config, db) => {
+  // UI routes
+  app.get('/', (_, res) => res.render('index'))
+
+  // Oauth routes
+
+  // API
+}
+
+/*
 module.exports = require('fs')
   .readdirSync(__dirname)
   .filter(file => file !== 'index.js')
   .map(filename => require(`${__dirname}/${filename}`));
+  */
