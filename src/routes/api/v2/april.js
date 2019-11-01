2a18ba3dfdd2c15b099104fa5aee28155a487692
Bowser65 <contact.bowser65@gmail.com>
2019-11-01 20:27:48 +0100

    4 is just above 1 ok

--- src/routes/api/v2/april.js
+++ src/routes/api/v2/april.js
@@ -1,6 +1,6 @@
 module.exports = async (req, res) => {
   const date = new Date();
-  if (date.getUTCMonth() === 1 && date.getUTCDay() === 1) {
+  if (date.getUTCMonth() === 4 && date.getUTCDay() === 1) {
     return res.json({ not: 'yet' });
   }
   res.json({ whats_this: req.config.aprilFools });
