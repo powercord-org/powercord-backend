bd8298626b7a34b05ed21d64ec4b9f36202403db
Cynthia <cynthia@cynthia.dev>
2020-11-27 20:21:43 +0100

    Small homepage text change

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -37,17 +37,17 @@ const Home = () => (
       <Section
         icon={Icons.Feather}
         title='Lightweight'
-        desc={'Powercord is designed to use low to no extra resources. You won\'t even notice it\'s here!'}
+        desc={'Powercord is designed to use low to no extra resources. You won\'t even notice it\'s here!*'}
       />
       <Section
         icon={Icons.Leaf}
         title='Native Feeling'
-        desc={'We put effort in making our UIs look the same as Discord\'s UIs and ensure high compatibility with themes'}
+        desc={'We put effort in making our UIs look the same as Discord\'s UIs and ensure high compatibility with themes.'}
       />
       <Section
         icon={Icons.Update}
         title='Built-in Updater'
-        desc={'Install your plugin and themes, and we\'ll take care of keeping them up to date'}
+        desc={'Get the newest features, the latest fixes, automatically. We even handle update conflicts for you!'}
       />
       <Section
         icon={Icons.API}
@@ -57,16 +57,20 @@ const Home = () => (
       <Section
         icon={Icons.Download}
         title='Plugin/Themes Installer'
-        desc={'Install your plugin and themes, and we\'ll take care of keeping them up to date'}
+        desc={'Browse plugins and themes, and install the ones you like by a simple click. All without leaving Discord.'}
         isSoon
       />
       <Section
         icon={Icons.Brush}
         title='Theme Customization'
-        desc={'Themes, redefined. Theme devs can let you make the theme *your* theme through an easy configuration screen'}
+        desc={'Themes, redefined. Theme devs can let you make the theme *your* theme through an easy configuration screen.'}
         isSoon
       />
     </section>
+    <p className={style.note}>
+      *Powercord still runs on top of the official Discord client, so it won't magically make it consume less
+      memory or CPU. However, we try our best to not consume even <i>more</i> resources.
+    </p>
     <section className={style.install}>
       <h2>What are you waiting for???</h2>
       <p>Make your Discord spicier. <Link to={Routes.INSTALLATION}>Install Powercord</Link>!</p>
