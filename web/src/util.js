b87fb5973d343e0c8532ef3fe1110d923b903231
Cynthia <cynthia@cynthia.dev>
2021-01-01 23:48:13 +0100

    Merry thanksgiving

--- web/src/util.js
+++ web/src/util.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020 aetheryx & Bowser65
+ * Copyright (c) 2018-2021 aetheryx & Cynthia K. Rey
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
