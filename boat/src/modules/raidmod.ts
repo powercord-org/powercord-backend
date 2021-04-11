860bf3d6209551f3afd73741fbc99627d098b3ee
BenSegal855 <ben@segals.org>
2021-04-11 17:48:35 -0400

    tweak: Switch faid kick to softbans

--- boat/src/modules/raidmod.ts
+++ boat/src/modules/raidmod.ts
@@ -21,26 +21,13 @@
  */
 
 import { createHash } from 'crypto'
-import { Collection, CommandClient, GuildTextableChannel, Message } from 'eris'
+import { CommandClient, GuildTextableChannel, Message } from 'eris'
 import config from '../config.js'
-import { ban, kick } from '../mod.js'
-
-class RaidMessage {
-  userId: string
-  channelId: string
-  id: string
-
-  constructor(msg: Message) {
-    this.id = msg.id
-    this.userId = msg.author.id
-    this.channelId = msg.channel.id
-  }
-}
+import { ban, softBan } from '../mod.js'
 
 // Any new members who send more than THRESHOLD messages with the same content, will be kicked
 const THRESHOLD = 2
 const raiderBuffer = new Map<string, number>()
-const messageBuffer = new Collection(RaidMessage)
 const DAY_MS = 24 * 36e5
 
 async function process(this: CommandClient, msg: Message<GuildTextableChannel>) {
@@ -48,38 +35,14 @@ async function process(this: CommandClient, msg: Message<GuildTextableChannel>) 
       || msg.member.joinedAt < Date.now() - DAY_MS
       || msg.member.createdAt > Date.now() - 5 * DAY_MS) return
 
-  addRaidMessage(msg)
-
   if (isRaider(msg.author.id, msg.content)) {
     if (await this.mongo.collection('raiders').countDocuments({ userId: msg.author.id }) > 0) {
       ban(msg.channel.guild, msg.author.id, this.user, 'Repeat raider', 0, 1)
     } else {
-      deleteRaiderMessages(this, msg.author.id)
-      kick(msg.channel.guild, msg.author.id, this.user, 'Detected raid spam')
+      softBan(msg.channel.guild, msg.author.id, this.user, 'Detected raid spam', 1)
       this.mongo.collection('raiders').insertOne({ userId: msg.author.id })
     }
   }
-}
-
-function addRaidMessage(msg: Message) {
-  messageBuffer.add(new RaidMessage(msg))
-  setTimeout(() => messageBuffer.remove({ id: msg.id }), 60e3)
-}
-
-function deleteRaiderMessages(bot: CommandClient, userId: string) {
-  const userMsgs = messageBuffer.filter(rm => rm.userId === userId)
-  const channels = new Map<string, Array<string>>()
-
-  userMsgs.forEach(rm => {
-    if (channels.has(rm.channelId)) {
-      channels.get(rm.channelId)?.push(rm.id)
-    } else {
-      channels.set(rm.channelId, [rm.id])
-    }
-  })
-
-  channels.forEach((messages, channel) => bot.deleteMessages(channel, messages, 'Detected raid spam'))
-
 }
 
 function isRaider(user: string, message: string): boolean {
