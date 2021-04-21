32f73ccb3538ab0a0fbd628f93966c14c31e193e
Cynthia <cynthia@cynthia.dev>
2021-04-21 16:53:32 +0200

    people can edit messages, right

--- boat/src/modules/automod.ts
+++ boat/src/modules/automod.ts
@@ -39,48 +39,51 @@ const INVITE_CHECK_FOR = [
 const EMOJI_RE = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff]|(?:<a?:[^:]{2,}:\d{6,}>))/g
 const MAX_EMOJI_THRESHOLD_MULTIPLIER = 0.3 // Amount of words * mult (floored) = max amount of emojis allowed
 
+async function process (this: CommandClient, msg: Message<GuildTextableChannel>) {
+  if (msg.guildID !== config.discord.ids.serverId || msg.author.bot) return null
+
+  // Filter ads
+  const invites = msg.content.match(INVITE_RE_G)
+  if (invites) {
+    for (const invite of invites) {
+      const [ , url, code ] = invite.match(INVITE_RE)!
+      if (INVITE_CHECK_FOR.includes(url)) {
+        const inv = await this.getInvite(code)
+        if (inv && inv.guild?.id === config.discord.ids.serverId) continue
+      }
+
+      skipSnipe.add(msg.id)
+      deleteMeta.set(msg.id, 'Advertisement')
+      msg.delete('Message contained advertisement')
+      msg.channel.createMessage({
+        content: `<@${msg.author.id}> **Rule #02**: Advertising of any kind is prohibited.`,
+        allowedMentions: { users: [ msg.author.id ] },
+      }).then((m) => setTimeout(() => m.delete(), 10e3))
+
+      return
+    }
+  }
+
+  // Filter emoji spam
+  const emojis = msg.content.match(EMOJI_RE)!.length
+  if (emojis > 5) {
+    const words = msg.content.replace(EMOJI_RE, '').split(/\s+/g).filter(Boolean).length
+    const max = Math.floor(words * MAX_EMOJI_THRESHOLD_MULTIPLIER)
+    if (emojis > max) {
+      skipSnipe.add(msg.id)
+      deleteMeta.set(msg.id, 'Emoji spam')
+      msg.delete('Message contained too many emojis')
+      msg.channel.createMessage({
+        content: `<@${msg.author.id}> **Rule #03**: Spam of any kind is prohibited.\nConsider reducing the amount of emojis in your message.`,
+        allowedMentions: { users: [ msg.author.id ] },
+      }).then((m) => setTimeout(() => m.delete(), 10e3))
+
+      return
+    }
+  }
+}
+
 export default function (bot: CommandClient) {
-  bot.on('messageCreate', async (msg: Message<GuildTextableChannel>) => {
-    if (msg.guildID !== config.discord.ids.serverId || msg.author.bot) return null
-
-    // Filter ads
-    const invites = msg.content.match(INVITE_RE_G)
-    if (invites) {
-      for (const invite of invites) {
-        const [ , url, code ] = invite.match(INVITE_RE)!
-        if (INVITE_CHECK_FOR.includes(url)) {
-          const inv = await bot.getInvite(code)
-          if (inv && inv.guild?.id === config.discord.ids.serverId) continue
-        }
-
-        skipSnipe.add(msg.id)
-        deleteMeta.set(msg.id, 'Advertisement')
-        msg.delete('Message contained advertisement')
-        msg.channel.createMessage({
-          content: `<@${msg.author.id}> **Rule #02**: Advertising of any kind is prohibited.`,
-          allowedMentions: { users: [ msg.author.id ] },
-        }).then((m) => setTimeout(() => m.delete(), 10e3))
-
-        return
-      }
-    }
-
-    // Filter emoji spam
-    const emojis = msg.content.match(EMOJI_RE)!.length
-    if (emojis > 5) {
-      const words = msg.content.replace(EMOJI_RE, '').split(/\s+/g).filter(Boolean).length
-      const max = Math.floor(words * MAX_EMOJI_THRESHOLD_MULTIPLIER)
-      if (emojis > max) {
-        skipSnipe.add(msg.id)
-        deleteMeta.set(msg.id, 'Emoji spam')
-        msg.delete('Message contained too many emojis')
-        msg.channel.createMessage({
-          content: `<@${msg.author.id}> **Rule #03**: Spam of any kind is prohibited.\nConsider reducing the amount of emojis in your message.`,
-          allowedMentions: { users: [ msg.author.id ] },
-        }).then((m) => setTimeout(() => m.delete(), 10e3))
-
-        return
-      }
-    }
-  })
+  bot.on('messageCreate', process)
+  bot.on('messageUpdate', process)
 }
