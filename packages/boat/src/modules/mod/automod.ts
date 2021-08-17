0c6bae71889becb7869329114bf26f8e114ff3b3
Cynthia <cynthia@cynthia.dev>
2021-08-17 16:51:38 +0200

    fix(boat): more unicode memes

--- packages/boat/src/modules/mod/automod.ts
+++ packages/boat/src/modules/mod/automod.ts
@@ -41,15 +41,15 @@ const INVITE_CHECK_FOR = [
   'watchanimeattheoffice.com/invite',
 ]
 
-const CLEANER = /\s|[\u200B-\u200D\uFEFF]|[\u0300-\u036f]/g
-const BAD_POWERCORD = /[Pp]ower[-_.]*([C(]|©️)ord/
+const CLEANER = /\s|[\u200B-\u200D\uFEFF]|[\u0300-\u036f]|[\u202A-\u202E]|[/\\]/g
+const BAD_POWERCORD = /[Pp]ower[-_.,;:!*]*[C(]ord/
 const EMOJI_UNICODE_RE = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff]|(?:<a?:[^:]{2,}:\d{6,}>))/g
 const EMOJI_RE = new RegExp(`${NAMES.map((n: string) => `:${n}:`).join('|').replace(/\+/g, '\\+')}|${EMOJI_UNICODE_RE.source}`, 'g')
 const MAX_EMOJI_THRESHOLD_MULTIPLIER = 0.3 // Amount of words * mult (floored) = max amount of emojis allowed
 const NORMALIZE: [ RegExp, string ][] = [
   [ /[АΑ]/g, 'A' ],
   [ /[ВΒ]/g, 'B' ],
-  [ /[C]/g, 'C' ],
+  [ /[CС]/g, 'C' ],
   [ /[ЕЁΕ]/g, 'E' ],
   [ /[НΗ]/g, 'H' ],
   [ /[І]/g, 'I' ],
@@ -65,7 +65,8 @@ const NORMALIZE: [ RegExp, string ][] = [
   [ /[Υ]/g, 'Y' ],
   [ /[Ζ]/g, 'Z' ],
   [ /[аα]/g, 'a' ],
-  [ /[с]/g, 'c' ],
+  [ /[с]|©️/g, 'c' ],
+  [ /[đ]/g, 'd' ],
   [ /[её]/g, 'e' ],
   [ /[9]/g, 'g' ],
   [ /[ıіι]/g, 'i' ],
