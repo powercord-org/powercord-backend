b02e45415f173c76460e0370d119382309cad06e
Cynthia <cynthia@cynthia.dev>
2022-03-01 15:05:19 +0100

    chore: add SPDX-License-Identifier

--- packages/api/src/api/backoffice/crudLegacy.ts
+++ packages/api/src/api/backoffice/crudLegacy.ts
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2018-2022 Powercord Developers
  * Licensed under the Open Software License version 3.0
+ * SPDX-License-Identifier: OSL-3.0
  */
 
 // todo: make safe enough and modular enough for usage in public routes
--- packages/web/src/components/backoffice/Store/Forms/Items.tsx
+++ packages/web/src/components/backoffice/Store/Forms/Items.tsx
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2018-2022 Powercord Developers
  * Licensed under the Open Software License version 3.0
+ * SPDX-License-Identifier: OSL-3.0
  */
 
 import type { FormHosting, FormPublish, FormVerification, StoreForm } from '@powercord/types/store'
